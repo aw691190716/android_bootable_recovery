214b0ae4ed2146eddeb54503b7ad4294b3a7ecce
OldDroid <pohl199885@gmail.com>
2017-06-08 15:02:35 +0200

    Add option to keep /vendor mounted

--- data.cpp
+++ data.cpp
@@ -836,6 +836,9 @@ void DataManager::SetDefaultValues()
 	mData.SetValue("tw_app_install_status", "0"); // 0 = no status, 1 = not installed, 2 = already installed
 #endif
 
+	// SuperSU workaround
+	mPersist.SetValue("tw_dont_unmount_vendor", "1");
+
 	pthread_mutex_unlock(&m_valuesLock);
 }
 
